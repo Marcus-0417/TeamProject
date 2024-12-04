e7551bfa13bfba580397b27c39beee51783ff18e
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-04 16:56:46 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,22 +25,30 @@ function App() {
       <main>
         {/* 第一層 */}
         <div id='layer1'>
-          <figure className='stamp'><img src="./Public/images/stamp-4.png" alt="" /></figure>
+          <div><img className='stamp' src="./Public/images/stamp-4.png" alt="" /></div>
           <div className='text'>
-          <p className='text-TW'>郵你真好</p>
-          <p className='text-EN'>Send a postcard to convey warmth.</p>
+            <p className='text-TW'>郵你真好</p>
+            <p className='text-EN'>Send a postcard to convey warmth.</p>
           </div>
-          <figure className='stamp'><img src="./Public/images/stamp-3.png" alt="" /></figure>
+          <div><img className='stamp' src="./Public/images/stamp-3.png" alt="" /></div>
           {/* 裝飾郵戳 */}
-          <figure id="stampLeft"><img src="./Public/images/postmark-1.png" alt="左郵戳" /></figure>
-          <figure id="stampRight"><img src="./Public/images/postmark-1.png" alt="右郵戳" /></figure>
+          <div><img id="stampLeft" src="./Public/images/postmark-1.png" alt="左郵戳" /></div>
+          <div><img id="stampRight" src="./Public/images/postmark-1.png" alt="右郵戳" /></div>
         </div>
 
         {/* 第二層 */}
         <div id='layer2'>
-        <figure><img src="./Public/images/stamp-5.png" alt="" /></figure>
-        <figure><img src="./Public/images/btn-1.jpg" alt="" /></figure>
-        <figure><img src="./Public/images/stamp-6.jpg" alt="" /></figure>
+          <div><img className='layer2-1' src="./Public/images/stamp-5.png" alt="" /></div>
+          <div className='layer2-2'>
+            <img src="./Public/images/btn-1.jpg" alt="" />
+            <div className='text'>
+            <p id='text1'>希望能在忙碌的生活中</p>
+            <p id='text2'>找到與你共頻的人</p>
+            <p id='text3'>一起分享生活中的美好</p>
+            </div>
+
+          </div>
+          <div><img className='layer2-3' src="./Public/images/stamp-6.jpg" alt="" /></div>
 
         </div>
       </main>
