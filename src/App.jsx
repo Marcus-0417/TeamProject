48dbddab3c4f4adebe6b18d1e4c294cdfa123fe1
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-04 13:40:02 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,18 +25,24 @@ function App() {
       <main>
         {/* 第一層 */}
         <div id='layer1'>
-          <figure><img src="./Public/images/stamp-4.png" alt="" /></figure>
-          <p>郵你真好</p>
-          <p>Send a postcard to convey warmth.</p>
-          <figure><img src="./Public/images/stamp-3.png" alt="" /></figure>
+          <figure className='stamp'><img src="./Public/images/stamp-4.png" alt="" /></figure>
+          <div className='text'>
+          <p className='text-TW'>郵你真好</p>
+          <p className='text-EN'>Send a postcard to convey warmth.</p>
+          </div>
+          <figure className='stamp'><img src="./Public/images/stamp-3.png" alt="" /></figure>
           {/* 裝飾郵戳 */}
           <figure id="stampLeft"><img src="./Public/images/postmark-1.png" alt="左郵戳" /></figure>
           <figure id="stampRight"><img src="./Public/images/postmark-1.png" alt="右郵戳" /></figure>
         </div>
 
+        {/* 第二層 */}
+        <div id='layer2'>
+        <figure><img src="./Public/images/stamp-5.png" alt="" /></figure>
+        <figure><img src="./Public/images/btn-1.jpg" alt="" /></figure>
+        <figure><img src="./Public/images/stamp-6.jpg" alt="" /></figure>
 
-
-
+        </div>
       </main>
     </body>
   )
