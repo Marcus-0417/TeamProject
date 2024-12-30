c5d2f6f41c965c957261e03a1764b27366edd238
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-30 14:25:50 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/TeamProject/' : '/',
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/TeamProject/' : '/',
+  // base: './',
   plugins: [react()],
 })
