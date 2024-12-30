d3a0658ae30a8f7310ef9b3dd9f85ff12676a915
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-30 13:20:43 +0800

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                         <li><a href="/PC-index.html">配對明信片</a></li>
                         {/* <li><a href="">商店</a></li> */}
                         <li><a href="/Member-Index.html">會員專區</a></li>
-                        <li><a href="/index.html">關於</a></li>
+                        <li><a href="/about.html">關於</a></li>
                     </ul>
                 </nav>
             </header>
