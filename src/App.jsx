a13d9c47ed157fb5c82849a3462eed2c5e0bc853
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-30 14:24:14 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import MemStory from "./MemStory"
 import MainPage from "./MainPage";
 import MemStoryPage from "./MemStoryPage";
 
 const App = () => {
   return (
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
         <Route path="/MemStory" element={<MemStory />} />
         <Route path="/MemStoryPage" element={<MemStoryPage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
