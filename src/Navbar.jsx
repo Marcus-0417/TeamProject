35209ac85725c8194e56d9af8f114e1806c389cf
vicqq00qqq <vicqq00qqq@gmail.com>
2024-12-30 16:08:53 +0800

    update

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -15,10 +15,10 @@ const Navbar = () => {
                 <nav>
                     <ul className="menu">
                         <li><Link to="/">首頁</Link></li>
-                        <li><a href="/PC-index.html">配對明信片</a></li>
+                        <li><a href="./PC-index.html">配對明信片</a></li>
                         {/* <li><a href="">商店</a></li> */}
-                        <li><a href="/Member-Index.html">會員專區</a></li>
-                        <li><a href="/about.html">關於</a></li>
+                        <li><a href="./Member-Index.html">會員專區</a></li>
+                        <li><a href="./about.html">關於</a></li>
                     </ul>
                 </nav>
             </header>
