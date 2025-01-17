f7ed3890c0652d64859ad868eda9b76b231903bc
yuxzi <kitty841002@gmail.com>
2025-01-17 16:55:33 +0800

    rwd

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -9,10 +9,10 @@ export default function About(){
     <div id="AboutPage">
     <Navbar/>
     {/* 內容區 */}
-    <main>
-    <div className="wrap">
+    <main id="About-main">
+    <div className="About-wrap">
       {/* 左側按鈕  */}
-      <div className="btn ">
+      <div className="About-btn ">
       <Link to="/About"><button className="button-grow-shadow">關於</button></Link>
       <Link to="/Qa"><button className="button-grow-shadow">FAQ</button></Link>
         <Link to="/Content"><button className="button-grow-shadow">聯絡我們</button></Link>
@@ -20,8 +20,8 @@ export default function About(){
       </div>
 
       {/* 中間內容 */}
-      <div className="main">
-        <div className="title">
+      <div className="About-main-box">
+        <div className="About-title">
           <h2>
             <span className="block block1">關</span>
             <span className="block block2">於</span>
@@ -32,31 +32,37 @@ export default function About(){
           </h2>
         </div>
 
-        <div className="info">
-          <div className="box1">
+        <div className="About-info">
+          <div className="About-box1">
+            <div className="About-box1-content">
             <h3>郵你真好是一個什麼樣的平台?</h3>
             <p>為了喜歡使用實體卡片與別人交流的人所設計的平台，在這裡可以獲得良好情緒價值、找到同頻的好夥伴進行友好交流，我們希望在這科技發達的時代，依然能夠使用實體明信片並配上手寫字，創造有溫度的交流。
             </p>
+            </div>
+           
           </div>
 
-          <div className="box1b">
+          <div className="About-box1b">
+            <div className="About-box2-content">
             <h3>郵你真好是如何誕生的呢?</h3>
             <p>為了喜歡使用實體卡片與別人交流的人所設計的平台，在這裡可以獲得良好情緒價值、找到同頻的好夥伴進行友好交流，我們希望在這科技發達的時代，依然能夠使用實體明信片並配上手寫字，創造有溫度的交流。
             </p>
+            </div>
+           
           </div>
         </div>
       </div>
 
       {/* 裝飾區  */}
-      <figure className="pic1"><img src="./images/tape-1.png" alt=""/></figure>
-      <figure className="pic2"><img src="./images/tape-1.png" alt=""/></figure>
-      <figure className="pic3"><img src="./images/postmask-5.png" alt=""/></figure>
-      <figure className="pic4"><img src="./images/flowers-2.png" alt=""/></figure>
-      <figure className="pic5"><img src="./images/postmark-1.png" alt=""/></figure>
-      <figure className="pic6"><img src="./images/small_paper-3.png" alt=""/></figure>
-      <figure className="pic7"><img src="./images/flowers-3.png" alt=""/></figure>
-      <figure className="pic8"><img src="./images/tape-2.png" alt=""/></figure>
-      <figure className="pic9"><img src="./images/tape-2.png" alt=""/></figure>
+      <figure className="About-pic1"><img src="./images/tape-1.png" alt=""/></figure>
+      <figure className="About-pic2"><img src="./images/tape-1.png" alt=""/></figure>
+      <figure className="About-pic3"><img src="./images/postmask-5.png" alt=""/></figure>
+      <figure className="About-pic4"><img src="./images/flowers-2.png" alt=""/></figure>
+      <figure className="About-pic5"><img src="./images/postmark-1.png" alt=""/></figure>
+      <figure className="About-pic6"><img src="./images/small_paper-3.png" alt=""/></figure>
+      <figure className="About-pic7"><img src="./images/flowers-3.png" alt=""/></figure>
+      <figure className="About-pic8"><img src="./images/tape-2.png" alt=""/></figure>
+      <figure className="About-pic9"><img src="./images/tape-2.png" alt=""/></figure>
     </div>
   </main>
 
--- src/about/Content.jsx
+++ src/about/Content.jsx
@@ -7,10 +7,10 @@ export default function Content() {
       <Navbar />
 
       {/* 內容區 */}
-      <main>
-        <div className="wrap">
+      <main id="contentPage-main">
+        <div className="contentPage-wrap">
           {/* 左側按鈕  */}
-          <div className="btn ">
+          <div className="contentPage-btn ">
             <Link to="/About"><button className="button-grow-shadow">關於</button></Link>
             <Link to="/Qa"><button className="button-grow-shadow">FAQ</button></Link>
             <Link to="/Content"><button className="button-grow-shadow">聯絡我們</button></Link>
@@ -18,31 +18,31 @@ export default function Content() {
           </div>
 
           {/* 內容區  */}
-          <div className="info2-box">
-            <div className="box1c">
+          <div className="contentPage-info2-box">
+            <div className="contentPage-box1c">
               <h2>請在此處留下您的問題與聯繫方式，我們將盡速與您聯繫。</h2>
             </div>
 
-            <div className="info2">
-              <div className="box2c">
+            <div className="contentPage-info2">
+              <div className="contentPage-box2c">
                 <h3>名字</h3>
                 <input type="text"/>
               </div>
-              <div className="box3c">
+              <div className="contentPage-box3c">
                 <h3>E-mail</h3>
                 <input type="text"/>
               </div>
-              <div className="box4c">
+              <div className="contentPage-box4c">
                 <h3>主旨</h3>
                 <input type="text"/>
               </div>
-              <div className="box5c">
+              <div className="contentPage-box5c">
                 <h3>內容</h3>
                 <input type="text"/>
               </div>
             </div>
 
-            <div className="box6c">
+            <div className="contentPage-box6c">
               <button>
                 <p>送出</p>
               </button>
@@ -52,13 +52,13 @@ export default function Content() {
 
 
         {/* 裝飾區   */}
-        <figure class="pic1"><img src="./images/tape-1.png" alt=""/></figure>
-        <figure class="pic2"><img src="./images/tape-1.png" alt=""/></figure>
-        <figure class="pic3"><img src="./images/postmask-5.png" alt=""/></figure>
-        <figure class="pic4"><img src="./images/flowers-2.png" alt=""/></figure>
-        <figure class="pic5"><img src="./images/postmark-1.png" alt=""/></figure>
-        <figure class="pic6"><img src="./images/small_paper-3.png" alt=""/></figure>
-        <figure class="pic7"><img src="./images/flowers-3.png" alt=""/></figure>
+        <figure class="contentPage-pic1"><img src="./images/tape-1.png" alt=""/></figure>
+        <figure class="contentPage-pic2"><img src="./images/tape-1.png" alt=""/></figure>
+        <figure class="contentPage-pic3"><img src="./images/postmask-5.png" alt=""/></figure>
+        <figure class="contentPage-pic4"><img src="./images/flowers-2.png" alt=""/></figure>
+        <figure class="contentPage-pic5"><img src="./images/postmark-1.png" alt=""/></figure>
+        <figure class="contentPage-pic6"><img src="./images/small_paper-3.png" alt=""/></figure>
+        <figure class="contentPage-pic7"><img src="./images/flowers-3.png" alt=""/></figure>
 
       </main>
 
